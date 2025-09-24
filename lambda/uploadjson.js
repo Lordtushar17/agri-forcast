c47769c4489161209aba3b1272b8a58f43669a89
FebArch <febarchapray@gmail.com>
2025-09-24 12:46:08 +0530

    datatta.json

---
+++ lambda/uploadjson.js
@@ -0,0 +1,31 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const data = require("./data.json");
+
+const client = new DynamoDBClient({ region: "ap-south-1" });
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const tableName = "YourDynamoDBTableName";
+
+async function uploadData() {
+  for (const item of data) {
+    const itemWithPK = {
+      id: Date.now().toString() + Math.random(),
+      ...item,
+    };
+    console.log("Uploading item:", itemWithPK);
+    try {
+      await ddbDocClient.send(
+        new PutCommand({
+          TableName: tableName,
+          Item: itemWithPK,
+        })
+      );
+      console.log(`Uploaded item with id: ${itemWithPK.id}`);
+    } catch (err) {
+      console.error("Error uploading item:", err);
+    }
+  }
+}
+
+uploadData();
